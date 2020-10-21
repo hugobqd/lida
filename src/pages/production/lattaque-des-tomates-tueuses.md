933aafe7b5ea5295fcbd14f23f989d4cc633ba4e
Hugo <hugobqdweb@laposte.net>
2020-10-21 11:25:26 +0200

    production

---
+++ src/pages/production/lattaque-des-tomates-tueuses.md
@@ -0,0 +1,93 @@
+---
+templateKey: production-post
+title: L'Attaque des tomates tueuses
+date: 1978-09-17T14:43:00.000Z
+director: John De Bello
+featuredimage: /img/1467469587_1.jpg
+trailer: https://www.youtube.com/watch?v=clqDkCJs19s
+vod: https://www.amazon.com/Attack-Killer-Tomatoes-Cindy-Charles/dp/B00YORDKP2
+"VOD_list,":
+  - vod_item: https://www.tenk.fr/les-films-de-la-head/do-you-know-akane-okai.html
+  - vod_item: https://vimeo.com/showcase/6562230/video/322417636
+gallery_list:
+  - gallery_img: /img/screen-shot-2019-07-19-at-3.06.45-pm.png
+    gallery_caption: Les héros
+  - gallery_img: /img/akt.jpg
+    gallery_caption: Ça provient d'un ***film***, *surement*.
+  - gallery_img: /img/killerhed.png
+  - gallery_img: /img/unnamed.jpg
+    gallery_caption: Oui oui ! C'est bien **George Clooney** !
+  - gallery_img: /img/n8jslmc8zt2p1relauyrcc3vn7b.jpg
+document_list:
+  - document_item: /img/licence_1_geo_e_le_ves.pdf
+  - document_item: /img/sujet-7-maya.pdf
+    document_title: Sujet 7
+technical: |-
+  Durée
+
+  Format
+
+  Année
+
+  Langue 
+
+  Genre
+selection: |-
+  Return of the Killer Tomatoes! (1988)
+  Killer Tomatoes Strike Back! (1990)
+  Killer Tomatoes Eat France! (1991)
+featuredpost: true
+productor: >-
+  The finished film contains footage of a real helicopter crash. In a scene
+  showing law enforcement officers firing their weapons to ward off tomatoes in
+  a field, a $60,000 [Hiller
+  Aircraft](https://en.wikipedia.org/wiki/Hiller_Aircraft "Hiller Aircraft")
+  UH-12E that had been rented for the production was supposed to have landed in
+  the tomato patch behind the officers, but during the landing, its tail rotor
+  struck the ground, causing the craft to spin out of control near the ground,
+  roll over, and burst into flames. 
+
+
+  The helicopter pilot escaped without serious injury.
+tags:
+  - fiction
+description: The film opens with a scroll saying that when Alfred Hitchcock's
+  film *The Birds (1963)* was released, audiences laughed at the notion of birds
+  revolting against humanity, but when an attack perpetrated by birds occurred
+  in 1975, no one laughed. This is followed by a pre credits sequence of a
+  tomato rising out of a woman's garbage disposal. Her *puzzlement* turns into
+  terror as the tomato draws her into a corner. Following the credits, the
+  police investigate her death. One officer discovers that the red substance
+  with which she is covered is not blood, **but tomato juice**.
+credit: |-
+  David Miller as Mason Dixon
+  George Wilson as Jim Richardson
+  Sharon Taylor as Lois Fairchild
+  Stephen Peace as Wilbur Finletter
+  Ernie Meyers as the President
+  Eric Christmas as Senator Polk
+  Ron Shapiro as Newspaper editor
+  Al Sklar as Ted Swan
+  Jerry Anderson as Major Mills
+  Jack Riley as Von Schauer
+  Gary Smith as Sam Smith
+  John Qualls as Captain
+  Geoff Ramsey as the Stapler
+  Ryan Shields as Tomato
+  Benita Barton as Gretta Attenbaum
+  Don Birch as the Old Man
+  Tom Coleman as the Singing Soldier
+  Art K. Koustik as the FIA Director
+  Jack Nolen as Senator McKinley
+  Paul Oya as Dr. Nokitofa
+  Robert Judd as the Stuntman
+  Byron Teegarden as Dr. Morrison
+  Michael Seewald as Cameraman
+  Steve Cates as Greg Colburn, the diver
+  Dean Grell as Miss Potato Famine 1922
+---
+Attack of the Killer Tomatoes is a 1978 American parody film\[2] produced by J. Stephen Peace and John DeBello, and directed by John DeBello based upon an original idea by Costa Dillon. The screenplay was written by Dillon, Peace, and DeBello. The film is a spoof of B movies. Made on a budget less than US$ 100,000 the story involves tomatoes becoming sentient by unknown means and revolting against humanity.
+
+## Plot
+
+The box office success of the film led to three sequels, all co-written by the same three writers and directed by DeBello.
---
+++ src/templates/production-post.js
@@ -0,0 +1,232 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { kebabCase } from "lodash";
+import { Helmet } from "react-helmet";
+import { graphql, Link } from "gatsby";
+import Layout from "../components/Layout";
+import Content, { HTMLContent } from "../components/Content";
+import Container from "../components/Container";
+import PreviewCompatibleImage from "../components/PreviewCompatibleImage";
+import Heading from "../components/Heading";
+import styled from "styled-components";
+import ReactPlayer from "react-player";
+import { Row, Col } from "../components/GridSystem";
+import Box from "../components/Box";
+
+export const ProductionPostTemplate = ({
+  content,
+  contentComponent,
+  credit,
+  date,
+  description,
+  director,
+  featuredimage,
+  gallery_list,
+  helmet,
+  productor,
+  selection,
+  tags,
+  technical,
+  title,
+  trailer,
+  vod,
+}) => {
+  const PostContent = contentComponent || Content;
+
+  return (
+    <main>
+      {helmet || ""}
+      <Container>
+        <Heading mb={3}>{title}</Heading>
+      </Container>
+      {/* {featuredimage && ( */}
+      <PreviewCompatibleImage
+        imageInfo={{
+          image: featuredimage,
+          alt: `Image principale de ${title}`,
+        }}
+      />
+      {/* )} */}
+      <Container mt={3}>
+        <Row>
+          <Col>
+            <h4 style={{ textTransform: "uppercase", fontWeight: 900 }}>
+              {director}
+            </h4>
+            {tags && tags.length && (
+              <span style={{ textTransform: "capitalize" }}>
+                {tags.map((tag) => (
+                  <span key={tag + `tag`}>{tag} —</span>
+                ))}
+              </span>
+            )}{" "}
+            {date}
+          </Col>
+          {vod && (
+            <Col style={{ textAlign: "right" }}>
+              <Link to={vod} target="_blank">
+                VOD
+              </Link>
+            </Col>
+          )}
+        </Row>
+      </Container>
+
+      <Container intro py={3} lineHeight={1.3}>
+        <p className="fs-4">{description}</p>
+      </Container>
+
+      <Container>
+        {trailer && (
+          <ReactPlayer
+            url={trailer}
+            width="100%"
+            height="500px"
+            controls="true"
+            light
+            playsinline
+          />
+        )}
+
+        {gallery_list &&
+          gallery_list.map((image) => (
+            <div style={{ marginTop: "2rem" }}>
+              <PreviewCompatibleImage
+                imageInfo={{
+                  image: image.gallery_img,
+                  alt: image.gallery_caption,
+                }}
+              />
+            </div>
+          ))}
+      </Container>
+
+      <Container text>
+        <PostContent content={content} />
+      </Container>
+
+      <Container py={5}>
+        <div style={{ fontSize: ".8em" }}>
+          <Row>
+            <Col span={3}>
+              <h4>Fiche technique</h4>
+              {technical}
+            </Col>
+            <Col span={3}>
+              <h4>Équipe</h4>
+              {credit}
+            </Col>
+            <Col span={3}>
+              <h4>Production</h4>
+              {productor}
+            </Col>
+            <Col span={3}>
+              <h4>Séléction</h4>
+              {selection}
+            </Col>
+          </Row>
+        </div>
+      </Container>
+    </main>
+  );
+};
+
+ProductionPostTemplate.propTypes = {
+  content: PropTypes.node.isRequired,
+  contentComponent: PropTypes.func,
+  description: PropTypes.string,
+  title: PropTypes.string,
+  helmet: PropTypes.object,
+};
+
+const ProductionPost = ({ data }) => {
+  const { markdownRemark: post } = data;
+
+  return (
+    <Layout>
+      <pre
+        style={{
+          background: "navy",
+          fontSize: 10,
+          display: "none",
+        }}
+      >
+        {JSON.stringify(data, null, 2)}
+      </pre>
+      <ProductionPostTemplate
+        post={post}
+        content={post.html}
+        contentComponent={HTMLContent}
+        description={post.frontmatter.description}
+        helmet={
+          <Helmet titleTemplate="%s | Film">
+            <title>{`${post.frontmatter.title}`}</title>
+            <meta
+              name="description"
+              content={`${post.frontmatter.description}`}
+            />
+          </Helmet>
+        }
+        credit={post.frontmatter.credit}
+        date={post.frontmatter.date}
+        director={post.frontmatter.director}
+        featuredimage={post.frontmatter.featuredimage}
+        gallery_list={post.frontmatter.gallery_list}
+        productor={post.frontmatter.productor}
+        selection={post.frontmatter.selection}
+        tags={post.frontmatter.tags}
+        title={post.frontmatter.title}
+        trailer={post.frontmatter.trailer}
+        vod={post.frontmatter.vod}
+      />
+    </Layout>
+  );
+};
+
+ProductionPost.propTypes = {
+  data: PropTypes.shape({
+    markdownRemark: PropTypes.object,
+  }),
+};
+
+export default ProductionPost;
+
+export const pageQuery = graphql`
+  query ProductionPostByID($id: String!) {
+    markdownRemark(id: { eq: $id }) {
+      id
+      html
+      frontmatter {
+        credit
+        date(formatString: "YYYY")
+        description
+        director
+        featuredimage {
+          childImageSharp {
+            fluid(maxWidth: 2000, quality: 100) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        gallery_list {
+          gallery_caption
+          gallery_img {
+            id
+            childImageSharp {
+              fluid {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+        }
+        productor
+        selection
+        tags
+        technical
+        title
+        trailer
+        vod
+      }
+    }
+  }
+`;
